d7dbdd33ddda2e39c82926bae11cc399502e26b8
thkocks <timhkocks@gmail.com>
2017-06-07 11:54:14 +0200

    redirect user/admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if current_user.admin? == true
+      rails_info_path
+    else
+      root_path
+    end
+  end
 end
