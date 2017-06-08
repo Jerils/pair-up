67de2fb6a74108ffb0691456a99ef6cc9aa15123
thkocks <timhkocks@gmail.com>
2017-06-08 08:51:12 +0200

    Fix routes after pull request merge

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   root :to => redirect("/users/sign_in")
   devise_for :users
   resources :admins
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
