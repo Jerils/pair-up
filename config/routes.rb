aefa0a9dca362ccad8bf2b9690dcc8a0b9904f51
thkocks <timhkocks@gmail.com>
2017-06-08 08:45:17 +0200

    Fix routes after branch merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   authenticated :user do
-    root :to => redirect("/users/sign_in")
+    root :to => "pages#home"
   end
 
+  root :to => redirect("/users/sign_in")
   devise_for :users
   resources :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
