c1f965e9776bd8b2a38aa608279084cc21917734
jeroen-ro <jeroen.rodenburg@outlook.com>
2017-06-09 14:55:09 +0200

    styling and links

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,4 +1,18 @@
 class Pair < ApplicationRecord
   belongs_to :user
 
+  def self.calculate_table
+    users = User.all
+    users_col1 = []
+    users_col2 = []
+
+    users_col << users[0..6]
+    a = users_col.shuffle
+  end
+
+  def self.create_pairs
+    users = calculate_table
+    Pair.create(users: user_id)
+  end
+
 end
